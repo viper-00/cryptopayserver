2b6b8e57be005da53ad01e3ff6ff7e2c3a6edc96
viper-00 <zhongmingyang2000@gmail.com>
2024-10-14 05:06:41 +0800

    update: core of solana

--- src/packages/web3/chain/solana.ts
+++ src/packages/web3/chain/solana.ts
@@ -1,1 +1,32 @@
-export class SOLANA {}
+import { CHAINIDS, CHAINS } from 'packages/constants/blockchain';
+import { ChainAccountType } from '../types';
+import { Keypair } from '@solana/web3.js';
+
+export class SOLANA {
+  static chain = CHAINS.SOLANA;
+
+  static getChainIds(isMainnet: boolean): CHAINIDS {
+    return isMainnet ? CHAINIDS.SOLANA : CHAINIDS.SOLANA_DEVNET;
+  }
+
+  static createAccountBySeed(isMainnet: boolean, seed: Buffer): ChainAccountType {
+    // const path = `m/44'/501'/0'/0`;
+
+    try {
+      const keypair = Keypair.fromSeed(Uint8Array.from(seed));
+      const privateKey = keypair.secretKey.toString();
+      const address = keypair.publicKey.toString();
+
+      return {
+        chain: this.chain,
+        address: address,
+        privateKey: privateKey,
+        note: 'Solana',
+        isMainnet: isMainnet,
+      };
+    } catch (e) {
+      console.error(e);
+      throw new Error('can not create a wallet of solana');
+    }
+  }
+}
--- task.js
+++ task.js
@@ -1,20 +1,20 @@
-import cron from 'node-cron';
-import axios from 'axios';
+// import cron from 'node-cron';
+// import axios from 'axios';
 
-const baseUrl = 'http://127.0.0.1:8888/api';
+// const baseUrl = 'http://127.0.0.1:8888/api';
 
-cron.schedule('*/60 * * * * *', async () => {
-  try {
-    await axios.get(baseUrl + '/scheduler_order_expired');
-  } catch (e) {
-    console.error(e);
-  }
-});
+// cron.schedule('*/60 * * * * *', async () => {
+//   try {
+//     await axios.get(baseUrl + '/scheduler_order_expired');
+//   } catch (e) {
+//     console.error(e);
+//   }
+// });
 
-cron.schedule('*/10 * * * * *', async () => {
-  try {
-    await axios.get(baseUrl + '/scheduler_blockscan');
-  } catch (e) {
-    console.error(e);
-  }
-});
+// cron.schedule('*/10 * * * * *', async () => {
+//   try {
+//     await axios.get(baseUrl + '/scheduler_blockscan');
+//   } catch (e) {
+//     console.error(e);
+//   }
+// });
