86f0148ab1ed4232c67c5a610ed4b30797d90455
viper-00 <zhongmingyang2000@gmail.com>
2024-12-16 04:33:12 +0800

    update: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { APP_NAME } from 'packages/constants';
+import { APP_NAME, STATIC_ASSETS } from 'packages/constants';
 import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/v13-pagesRouter';
 
 export default function MyDocument(props: any) {
@@ -9,6 +9,19 @@ export default function MyDocument(props: any) {
         <DocumentHeadTags {...props} />
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        {/* <link
+          rel="icon"
+          href={`${STATIC_ASSETS}/brand/logo.svg`}
+          type="image/svg+xml"
+        /> */}
+        <link rel="icon" href={`${STATIC_ASSETS}/favicon.ico`} type="image/x-icon" />
+        <link rel="shortcut icon" href={`${STATIC_ASSETS}/favicon.ico`} />
+        <meta name="theme-color" content="#ffffff" />
+        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
       </Head>
       <body style={{ backgroundColor: '#f8f9fa' }}>
         <Main />
